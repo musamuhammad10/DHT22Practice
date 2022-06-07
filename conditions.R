bd334d917497a74be022228c17e24a006015af88
musamuhammad10 <muhammadmusa1004@gmail.com>
2022-06-07 18:33:21 -0400

    if statements

---
+++ conditions.R
@@ -0,0 +1,32 @@
+#snippets for if conditions
+
+userage <- 15
+
+# if user is younger than 21 tell them to drink lemonade
+# else offer them beer
+
+if (userage < 21){
+  print("Here is your lemonade...")
+} else {
+  print("Here is your beer...")
+}
+
+speed <- 50
+
+# if speed is < 40, you are slow
+# if speed is between 40-70, you are at residential area
+# if speed is larger than 120, you get a ticket
+# is between 70 and 120, you are on the highway
+
+#make sure the statements are in chronological order otherwise R will output the wrong bucket of the if statement
+# make sure the order of the buckets is correct - ascending or descending
+
+if (speed < 40){
+  print("you are slow")
+} else if (speed <= 70) {
+  print(" you are at a residential area")
+} else if (speed <= 120) {
+  print("you are on the highway")
+} else {
+  print("ticket")
+}
