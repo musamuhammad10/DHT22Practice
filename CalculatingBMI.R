369a7d4c32772f26cfab7bdcbc13196cc886a328
musamuhammad10 <muhammadmusa1004@gmail.com>
2022-06-07 20:24:25 -0400

    Adding categories

--- CalculatingBMI.R
+++ CalculatingBMI.R
@@ -13,6 +13,15 @@ if (is.na(weight) | is.na(height)) {
   print("Invalid entry")
 } else {
   # if valid, calculating and displaying users BMI
-  BMI <- weight / height^2
-  print(paste("Your BMI is:", BMI))
+  
+  if (BMI <= 18.5) {
+    BMI <- weight / height^2
+    print(paste("you are underweight, your BMI is", BMI))
+  } else if (BMI <= 25) {
+    BMI <- weight / height^2
+    print(paste("you are neither underweight or overweight, your BMI is", BMI))
+  } else {
+    BMI <- weight / height^2
+    print(paste("you are overweight, your BMI is", BMI))
+  }
 }
